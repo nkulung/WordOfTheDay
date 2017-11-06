4d4d01f4350035118662386010a3cab92ce589bd
nkulung <nkulung334@gmail.com>
2017-11-06 10:22:28 -0500

    improving code
    code updates

--- wordOfTheDay.py
+++ wordOfTheDay.py
@@ -2,19 +2,23 @@ import requests
 from bs4 import BeautifulSoup as bs
 import urllib3
 
+def getWord():
+    s = requests.get('http://dictionary.reference.com/wordoftheday/').text
+    soup = bs(s, "html.parser")
+    wordDay = soup.find("div", {"class": "definition-header"})
+    wordRaw = wordDay.text
+    wordList = []
+    wordRaw = wordRaw.split()
+    return wordRaw
+def main():
+    word = getWord()
+    word = word[2]
+    print('The Word of the Day is:',word)
+    input('Press ENTER to Exit')
 
+main()
 
-
-s = requests.get('http://dictionary.reference.com/wordoftheday/').text
-soup = bs(s, "html.parser")
-wordDay = soup.find("div", {"class": "definition-header"})
-wordRaw = wordDay.text
-wordList = []
-wordRaw = wordRaw.split()
-
-print('The Word of the Day is:',wordRaw[2])
-
-input('Press ENTER to exit')
+    
 
 
 
